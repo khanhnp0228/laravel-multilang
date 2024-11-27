6b223d9074f8af75558428d2b2a2907fdfbbdee8
Khanh Nguyen <khanh@propolife.co.jp>
2024-11-27 15:36:47 +0700

    Method #1: Laravel traditional resources/lang/xx folder with trans() method

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,5 @@
+<?php
+    return [
+        'page_title' => 'QuickAdmin page',
+        'page_description' => 'Best page in the world'
+    ];
---
+++ resources/lang/vi/messages.php
@@ -0,0 +1,5 @@
+<?php
+    return [
+        'page_title' => 'Trang quản trị',
+        'page_description' => 'Chất lượng tốt nhất'
+    ];
